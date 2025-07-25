1aaab90e6d78ceb223835012db28349aa909abf2
Sameer <sameer.edu19@gmail.com>
2025-07-25 12:53:23 +0530

    interchanged pages

--- app/page.tsx
+++ app/page.tsx
@@ -326,131 +326,7 @@ export default function AstroQuizPage() {
     );
   };
 
-  const renderInfoForm = () => (
 
-    <div className='bg-[url(/QP2.webp)] px-4 min-h-screen flex flex-col justify-center items-center '>
-      <header className='absolute top-4 left-4 z-50'>
-        <Image alt='logo' src={"Logo.webp"} className='h-24 w-24' width={150} height={150}></Image>
-      </header>
-
-      {/* Form API Call  */}
-
-      {gender === 'male' ? (
-        <>
-
-          <form method="post" className="bg-[rgba(255,255,255,0.42)] p-6 sm:p-8 rounded-4xl w-full max-w-xl border border-white/20 shadow-xl text-center" acceptCharset="UTF-8" action="https://www.aweber.com/scripts/addlead.pl" >
-
-            <div className='bg-[url(/Box1.webp)] bg-center rounded-xl'>
-              <h2 className="font-bebas text-xl sm:text-3xl py-2 tracking-wider mb-4">Enter Your Info</h2>
-            </div>
-
-            <div className='hidden'>
-              <input type="hidden" name="meta_web_form_id" value="931623795" />
-              <input type="hidden" name="meta_split_id" value="" />
-              <input type="hidden" name="listname" value="awlist6899920" />
-              <input type="hidden" name="redirect" value="https://biblecharacterquiz.com/thank-you/m" id="redirect_aa440fc14656c1e387b8c5b6942dff3c" />
-              <input type="hidden" name="meta_redirect_onlist" value="https://biblecharacterquiz.com/thank-you/m" />
-              <input type="hidden" name="meta_adtracking" value="Quiz_Funnel_Sign_Up_Form_(Male)" />
-              <input type="hidden" name="meta_message" value="1" />
-              <input type="hidden" name="meta_required" value="name,email" />
-
-
-
-              <input type="hidden" name="meta_tooltip" value="" />
-            </div>
-            <div id="af-form-931623795" className="af-form"><div id="af-body-931623795" className="af-body af-standards">
-              <div className="af-element">
-                <label className="previewLabel hidden" htmlFor="awf_field-118217367">Name:</label>
-                <div className="af-textWrap">
-                  <input id="awf_field-118217367" {...register('name')} placeholder='Name' type="text" name="name" className="w-full py-3 px-4 border text-left  font-dmSans font-bold dark-brown border-white/30 bg-[rgba(255,255,255,0.42)] hover:bg-[rgba(255,255,255,0.20)] rounded-xl text-sm sm:text-xl mb-4" tabIndex={500} />
-                </div>
-                <div className="af-clear"></div>
-              </div><div className="af-element">
-                <label className="previewLabel hidden " htmlFor="awf_field-118217368">Email:</label>
-                <div className="af-textWrap">
-                  <input className="w-full py-3 px-4 border text-left  font-dmSans font-bold dark-brown border-white/30 bg-[rgba(255,255,255,0.42)] hover:bg-[rgba(255,255,255,0.20)] rounded-xl text-sm sm:text-xl mb-4" id="awf_field-118217368" type="email" tabIndex={501} placeholder='Email' {...register('email')} />
-                </div><div className="af-clear"></div>
-              </div><div className="af-element buttonContainer">
-                <input name="submit" className="w-full py-3 px-4 bg-yellow-500 hover:bg-yellow-600 text-black rounded-xl disabled:opacity-50 disabled:cursor-not-allowed text-sm sm:text-base" type="submit" value="Submit" tabIndex={502} disabled={
-                  !watch('name') ||
-                  !watch('email')
-                } />
-                <button
-                  onClick={resetQuiz}
-                  type="button"
-                  className="mt-4 text-white block w-full text-center cursor-pointer text-sm hover:text-yellow-300"
-                >
-                  ← Start Over
-                </button>
-                <div className="af-clear"></div>
-              </div>
-            </div>
-            </div>
-            <div className='hidden'><Image width={0} height={0} src="https://forms.aweber.com/form/displays.htm?id=nMyMbEzM7Jys" alt="logo" /></div>
-
-          </form>
-
-
-        </>
-      ) : (<>
-
-        <form method="post" className="bg-[rgba(255,255,255,0.42)] p-6 sm:p-8 rounded-4xl w-full max-w-xl border border-white/20 shadow-xl text-center" acceptCharset="UTF-8" action="https://www.aweber.com/scripts/addlead.pl" >
-
-          <div className='bg-[url(/Box1.webp)] bg-center rounded-xl'>
-            <h2 className="font-bebas text-xl sm:text-3xl py-2 tracking-wider mb-4">Enter Your Info</h2>
-          </div>
-
-          <div className='hidden'>
-            <input type="hidden" name="meta_web_form_id" value="359719133" />
-            <input type="hidden" name="meta_split_id" value="" />
-            <input type="hidden" name="listname" value="awlist6899921" />
-            <input type="hidden" name="redirect" value="https://biblecharacterquiz.com/thank-you/f" id="redirect_cae1bfd88dc067218ddb2f9d7a611f72" />
-            <input type="hidden" name="meta_redirect_onlist" value="https://biblecharacterquiz.com/thank-you/f" />
-            <input type="hidden" name="meta_adtracking" value="Quiz_Funnel_Sign_Up_Form_(Female)" />
-            <input type="hidden" name="meta_message" value="1" />
-            <input type="hidden" name="meta_required" value="name,email" />
-
-
-
-            <input type="hidden" name="meta_tooltip" value="" />
-          </div>
-          <div id="af-form-359719133" className="af-form"><div id="af-body-359719133" className="af-body af-standards">
-            <div className="af-element">
-              <label className="previewLabel hidden" htmlFor="awf_field-118219805">Name:</label>
-              <div className="af-textWrap">
-                <input id="awf_field-118219805" {...register('name')} placeholder='Name' type="text" name="name" className="w-full py-3 px-4 border text-left  font-dmSans font-bold dark-brown border-white/30 bg-[rgba(255,255,255,0.42)] hover:bg-[rgba(255,255,255,0.20)] rounded-xl text-sm sm:text-xl mb-4" tabIndex={500} />
-              </div>
-              <div className="af-clear"></div>
-            </div><div className="af-element">
-              <label className="previewLabel hidden " htmlFor="awf_field-118219806">Email:</label>
-              <div className="af-textWrap">
-                <input className="w-full py-3 px-4 border text-left  font-dmSans font-bold dark-brown border-white/30 bg-[rgba(255,255,255,0.42)] hover:bg-[rgba(255,255,255,0.20)] rounded-xl text-sm sm:text-xl mb-4" id="awf_field-118219806" type="email" tabIndex={501} placeholder='Email' {...register('email')} />
-              </div><div className="af-clear"></div>
-            </div><div className="af-element buttonContainer">
-              <input name="submit" className="w-full py-3 px-4 bg-yellow-500 hover:bg-yellow-600 text-black rounded-xl disabled:opacity-50 disabled:cursor-not-allowed text-sm sm:text-base" type="submit" value="Submit" tabIndex={502} disabled={
-                !watch('name') ||
-                !watch('email')
-              } />
-              <button
-                onClick={resetQuiz}
-                type="button"
-                className="mt-4 text-white block w-full text-center cursor-pointer text-sm hover:text-yellow-300"
-              >
-                ← Start Over
-              </button>
-              <div className="af-clear"></div>
-            </div>
-          </div>
-          </div>
-          <div className='hidden'><Image width={0} height={0} alt='aweber' src="https://forms.aweber.com/form/displays.htm?id=zKyc7IycjMzM" />
-          </div>
-
-        </form>
-      </>)}
-
-    </div>
-
-  );
 
   const renderProduct = () => (
     <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.8 }}>
@@ -575,13 +451,138 @@ export default function AstroQuizPage() {
     </motion.div>
   );
 
+    const renderInfoForm = () => (
+
+    <div className='bg-[url(/QP2.webp)] px-4 min-h-screen flex flex-col justify-center items-center '>
+      <header className='absolute top-4 left-4 z-50'>
+        <Image alt='logo' src={"Logo.webp"} className='h-24 w-24' width={150} height={150}></Image>
+      </header>
+
+      {/* Form API Call  */}
+
+      {gender === 'male' ? (
+        <>
+
+          <form method="post" className="bg-[rgba(255,255,255,0.42)] p-6 sm:p-8 rounded-4xl w-full max-w-xl border border-white/20 shadow-xl text-center" acceptCharset="UTF-8" action="https://www.aweber.com/scripts/addlead.pl" >
+
+            <div className='bg-[url(/Box1.webp)] bg-center rounded-xl'>
+              <h2 className="font-bebas text-xl sm:text-3xl py-2 tracking-wider mb-4">Enter Your Info</h2>
+            </div>
+
+            <div className='hidden'>
+              <input type="hidden" name="meta_web_form_id" value="931623795" />
+              <input type="hidden" name="meta_split_id" value="" />
+              <input type="hidden" name="listname" value="awlist6899920" />
+              <input type="hidden" name="redirect" value="https://biblecharacterquiz.com/thank-you/" id="redirect_aa440fc14656c1e387b8c5b6942dff3c" />
+              <input type="hidden" name="meta_redirect_onlist" value="https://biblecharacterquiz.com/thank-you/" />
+              <input type="hidden" name="meta_adtracking" value="Quiz_Funnel_Sign_Up_Form_(Male)" />
+              <input type="hidden" name="meta_message" value="1" />
+              <input type="hidden" name="meta_required" value="name,email" />
+
+
+
+              <input type="hidden" name="meta_tooltip" value="" />
+            </div>
+            <div id="af-form-931623795" className="af-form"><div id="af-body-931623795" className="af-body af-standards">
+              <div className="af-element">
+                <label className="previewLabel hidden" htmlFor="awf_field-118217367">Name:</label>
+                <div className="af-textWrap">
+                  <input id="awf_field-118217367" {...register('name')} placeholder='Name' type="text" name="name" className="w-full py-3 px-4 border text-left  font-dmSans font-bold dark-brown border-white/30 bg-[rgba(255,255,255,0.42)] hover:bg-[rgba(255,255,255,0.20)] rounded-xl text-sm sm:text-xl mb-4" tabIndex={500} />
+                </div>
+                <div className="af-clear"></div>
+              </div><div className="af-element">
+                <label className="previewLabel hidden " htmlFor="awf_field-118217368">Email:</label>
+                <div className="af-textWrap">
+                  <input className="w-full py-3 px-4 border text-left  font-dmSans font-bold dark-brown border-white/30 bg-[rgba(255,255,255,0.42)] hover:bg-[rgba(255,255,255,0.20)] rounded-xl text-sm sm:text-xl mb-4" id="awf_field-118217368" type="email" tabIndex={501} placeholder='Email' {...register('email')} />
+                </div><div className="af-clear"></div>
+              </div><div className="af-element buttonContainer">
+                <input name="submit" className="w-full py-3 px-4 bg-yellow-500 hover:bg-yellow-600 text-black rounded-xl disabled:opacity-50 disabled:cursor-not-allowed text-sm sm:text-base" type="submit" value="Submit" tabIndex={502} disabled={
+                  !watch('name') ||
+                  !watch('email')
+                } />
+                <button
+                  onClick={resetQuiz}
+                  type="button"
+                  className="mt-4 text-white block w-full text-center cursor-pointer text-sm hover:text-yellow-300"
+                >
+                  ← Start Over
+                </button>
+                <div className="af-clear"></div>
+              </div>
+            </div>
+            </div>
+            <div className='hidden'><Image width={0} height={0} src="https://forms.aweber.com/form/displays.htm?id=nMyMbEzM7Jys" alt="logo" /></div>
+
+          </form>
+
+
+        </>
+      ) : (<>
+
+        <form method="post" className="bg-[rgba(255,255,255,0.42)] p-6 sm:p-8 rounded-4xl w-full max-w-xl border border-white/20 shadow-xl text-center" acceptCharset="UTF-8" action="https://www.aweber.com/scripts/addlead.pl" >
+
+          <div className='bg-[url(/Box1.webp)] bg-center rounded-xl'>
+            <h2 className="font-bebas text-xl sm:text-3xl py-2 tracking-wider mb-4">Enter Your Info</h2>
+          </div>
+
+          <div className='hidden'>
+            <input type="hidden" name="meta_web_form_id" value="359719133" />
+            <input type="hidden" name="meta_split_id" value="" />
+            <input type="hidden" name="listname" value="awlist6899921" />
+            <input type="hidden" name="redirect" value="https://biblecharacterquiz.com/thank-you/f" id="redirect_cae1bfd88dc067218ddb2f9d7a611f72" />
+            <input type="hidden" name="meta_redirect_onlist" value="https://biblecharacterquiz.com/thank-you/f" />
+            <input type="hidden" name="meta_adtracking" value="Quiz_Funnel_Sign_Up_Form_(Female)" />
+            <input type="hidden" name="meta_message" value="1" />
+            <input type="hidden" name="meta_required" value="name,email" />
+
+
+
+            <input type="hidden" name="meta_tooltip" value="" />
+          </div>
+          <div id="af-form-359719133" className="af-form"><div id="af-body-359719133" className="af-body af-standards">
+            <div className="af-element">
+              <label className="previewLabel hidden" htmlFor="awf_field-118219805">Name:</label>
+              <div className="af-textWrap">
+                <input id="awf_field-118219805" {...register('name')} placeholder='Name' type="text" name="name" className="w-full py-3 px-4 border text-left  font-dmSans font-bold dark-brown border-white/30 bg-[rgba(255,255,255,0.42)] hover:bg-[rgba(255,255,255,0.20)] rounded-xl text-sm sm:text-xl mb-4" tabIndex={500} />
+              </div>
+              <div className="af-clear"></div>
+            </div><div className="af-element">
+              <label className="previewLabel hidden " htmlFor="awf_field-118219806">Email:</label>
+              <div className="af-textWrap">
+                <input className="w-full py-3 px-4 border text-left  font-dmSans font-bold dark-brown border-white/30 bg-[rgba(255,255,255,0.42)] hover:bg-[rgba(255,255,255,0.20)] rounded-xl text-sm sm:text-xl mb-4" id="awf_field-118219806" type="email" tabIndex={501} placeholder='Email' {...register('email')} />
+              </div><div className="af-clear"></div>
+            </div><div className="af-element buttonContainer">
+              <input name="submit" className="w-full py-3 px-4 bg-yellow-500 hover:bg-yellow-600 text-black rounded-xl disabled:opacity-50 disabled:cursor-not-allowed text-sm sm:text-base" type="submit" value="Submit" tabIndex={502} disabled={
+                !watch('name') ||
+                !watch('email')
+              } />
+              <button
+                onClick={resetQuiz}
+                type="button"
+                className="mt-4 text-white block w-full text-center cursor-pointer text-sm hover:text-yellow-300"
+              >
+                ← Start Over
+              </button>
+              <div className="af-clear"></div>
+            </div>
+          </div>
+          </div>
+          <div className='hidden'><Image width={0} height={0} alt='aweber' src="https://forms.aweber.com/form/displays.htm?id=zKyc7IycjMzM" />
+          </div>
+
+        </form>
+      </>)}
+
+    </div>
+
+  );
 
 
   if (step === 'landing') return renderLanding();
   if (step === 'gender') return renderGender();
   if (isQuestionStep) return renderQuestion();
-  if (step === 'info') return renderInfoForm();
   if (step === 'product') return renderProduct();
+  if (step === 'info') return renderInfoForm();
   return null;
 }
 
